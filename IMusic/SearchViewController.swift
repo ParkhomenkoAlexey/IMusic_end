d79015fbef553cca09119652738a99f5be76bb5c
ParkhomenkoAlexey <Parkhomenko_AD@mail.ru>
2019-08-11 19:31:51 +0300

    Initial Commit

---
+++ IMusic/SearchViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SearchViewController.swift
+//  IMusic
+//
+//  Created by Алексей Пархоменко on 11/08/2019.
+//  Copyright © 2019 Алексей Пархоменко. All rights reserved.
+//
+
+import UIKit
+
+
+class SearchViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+    
+}
