aa25fa6beff147357d2666633710810fb20618bb
ParkhomenkoAlexey <Parkhomenko_AD@mail.ru>
2019-08-13 20:32:34 +0300

    9 урок. Закончили с VIP циклом у CleanSwift.

--- IMusic/Search/SearchViewController.swift
+++ IMusic/Search/SearchViewController.swift
@@ -21,6 +21,8 @@ class SearchViewController: UIViewController, SearchDisplayLogic {
     @IBOutlet weak var table: UITableView!
     
     let searchController = UISearchController(searchResultsController: nil)
+    private var searchViewModel = SearchViewModel.init(cells: [])
+    private var timer: Timer?
       
   // MARK: Setup
   
@@ -53,6 +55,7 @@ class SearchViewController: UIViewController, SearchDisplayLogic {
     private func setupSearchBar() {
         navigationItem.searchController = searchController
         navigationItem.hidesSearchBarWhenScrolling = false
+        searchController.obscuresBackgroundDuringPresentation = false
         searchController.searchBar.delegate = self
     }
     
@@ -65,8 +68,10 @@ class SearchViewController: UIViewController, SearchDisplayLogic {
     switch viewModel {
     case .some:
         print("viewController .some")
-    case .displayTracks:
+    case .displayTracks(let searchViewModel):
         print("viewController .displayTracks")
+        self.searchViewModel = searchViewModel
+        table.reloadData()
     }
   }
   
@@ -77,12 +82,16 @@ class SearchViewController: UIViewController, SearchDisplayLogic {
 extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return searchViewModel.cells.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = table.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
-        cell.textLabel?.text = "indexPath: \(indexPath)"
+        
+        let cellViewModel = searchViewModel.cells[indexPath.row]
+        cell.textLabel?.text = "\(cellViewModel.trackName)\n\(cellViewModel.artistName)"
+        cell.textLabel?.numberOfLines = 2
+        cell.imageView?.image = #imageLiteral(resourceName: "Image")
         return cell
     }
 }
@@ -90,6 +99,11 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
 extension SearchViewController: UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         print(searchText)
-        interactor?.makeRequest(request: Search.Model.Request.RequestType.some)
+    
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false, block: { (_) in
+            self.interactor?.makeRequest(request: Search.Model.Request.RequestType.getTracks(searchTerm: searchText))
+        })
+        
     }
 }
