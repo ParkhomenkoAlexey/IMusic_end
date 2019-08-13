47c7722ebc7d12e24195f009fb2208262095c763
ParkhomenkoAlexey <Parkhomenko_AD@mail.ru>
2019-08-13 22:26:11 +0300

    11 Урок. SDWebImage

--- IMusic/Services/NetworkService.swift
+++ IMusic/Services/NetworkService.swift
@@ -15,7 +15,7 @@ class NetworkService {
     func fetchTracks(searchText: String, competion: @escaping (SearchResponse?) -> Void)  {
         let url = "https://itunes.apple.com/search"
         let parameters = ["term":"\(searchText)",
-            "limit":"10",
+            "limit":"100",
             "media":"music"]
         
         Alamofire.request(url, method: .get, parameters: parameters, encoding: URLEncoding.default, headers: nil).responseData { (dataResponse) in
