3786d64d661c0110ffb50249056f757d20a2ce2f
ParkhomenkoAlexey <Parkhomenko_AD@mail.ru>
2019-08-14 19:02:33 +0300

    12 урок. header and footer views

--- IMusic/Services/NetworkService.swift
+++ IMusic/Services/NetworkService.swift
@@ -15,7 +15,7 @@ class NetworkService {
     func fetchTracks(searchText: String, competion: @escaping (SearchResponse?) -> Void)  {
         let url = "https://itunes.apple.com/search"
         let parameters = ["term":"\(searchText)",
-            "limit":"100",
+            "limit":"10",
             "media":"music"]
         
         Alamofire.request(url, method: .get, parameters: parameters, encoding: URLEncoding.default, headers: nil).responseData { (dataResponse) in
