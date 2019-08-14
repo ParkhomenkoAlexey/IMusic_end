bae03690821935da21dca8b6406191f42b8c5f79
ParkhomenkoAlexey <Parkhomenko_AD@mail.ru>
2019-08-14 21:43:01 +0300

    13 урок. Подгрузили TrackDetailView

---
+++ IMusic/Search/TrackDetailView/TrackDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  TrackDetailView.swift
+//  IMusic
+//
+//  Created by Алексей Пархоменко on 14/08/2019.
+//  Copyright © 2019 Алексей Пархоменко. All rights reserved.
+//
+
+import UIKit
+
+
+class TrackDetailView: UIView {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+}
